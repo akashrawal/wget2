14795b76cbd91e0ec6dbd6297b5eceef9e576e64
Akash Rawal <argmlac@gmail.com>
2017-06-12 16:46:22 +0530

    Moved nonexistent directory before exiting one, should stress the tests further

--- tests/test-plugin.c
+++ tests/test-plugin.c
@@ -147,8 +147,8 @@ int main(void)
 
 	//Check behavior for nonexistent search directories
 	wget_test(
-		WGET_TEST_OPTIONS, "--plugin-dirs=" OBJECT_DIR ","
-			OBJECT_DIR "/nonexistent --plugin=pluginname",
+		WGET_TEST_OPTIONS, "--plugin-dirs=" OBJECT_DIR "/nonexistent,"
+			OBJECT_DIR " --plugin=pluginname",
 		WGET_TEST_REQUEST_URL, "index.html",
 		WGET_TEST_EXPECTED_ERROR_CODE, 0,
 		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
