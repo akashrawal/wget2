a57484fab6eb31c48211abdcbe42f621f701b66f
Akash Rawal <argmlac@gmail.com>
2017-07-27 01:22:10 +0530

    * libwget/plugin.c: Improve documentation

--- libwget/plugin.c
+++ libwget/plugin.c
@@ -5,9 +5,7 @@
  *
  * Libwget is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  
  * Libwget is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -214,8 +212,8 @@ bool wget_downloaded_file_get_recurse(wget_downloaded_file_t *file)
 }
 
 /**
- * Adds a URL for recursive downloading.
- * This function has no effect if `wget_downloaded_file_get_recurse` returns false.
+ * Adds a URL for recursive downloading. This function has no effect if 
+ * \ref wget_downloaded_file_get_recurse "wget_downloaded_file_get_recurse()" returns false.
  *
  * \param[in] file Downloaded file handle
  * \param[in] iri The URL to be fetched.
